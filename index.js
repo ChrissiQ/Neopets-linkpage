f2e06fe91cb02f1b55a6a8a1f8995e0706fe6ff7
rediahs@gmail.com <rediahs@gmail.com>
2011-10-21 13:40:28 -0600

    Can now automate full list, however it's messy...

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-var addedLines = 0;
-// Function that adds a link list element.
-// containerListName is a string, the -ID- of the list we are adding elements to.  NOT the element itself.
+var addedLines = 0; //Must be global so that it remains constant outside of functions.
+
+
+// Function that adds a list node.
+// containerListID is the ID (as a string) of the container to add nodes to.
 // If locationToAdd = "start", link is added to start.  Otherwise, added to end.  "end" for convention.
+function addListNode(containerListID,locationToAdd){
 
-function addLink(containerListName,locationToAdd){
-	if (!containerListName){containerListName = "linklist"}	
-	// If locationToAdd isn't specified, or if it's not one of "start" or "end", make it "end" by default.
+	if (!containerListID){containerListID = "linklist"}						//Set default arguments.
 	if ((!locationToAdd) || (locationToAdd != "start" && locationToAdd != "end")){locationToAdd = "end"}
-	document.write(locationToAdd);
+	document.write(locationToAdd);						////////////////////// *** DEBUG CODE ***
 
-	/* ---- Elements pulled from document ------- */
-	/* Parent of list entry (ul) */
-	var existingListContainer = document.getElementById(containerListName);
-	/* Spacer which must remain at end of list (we are trying to insert before it when we add an entry). */
-	var endingSpacer = document.getElementById('endingSpacer');
-	
-	
-	/* ----- New elements we are creating for this function! ------- */
-	
+	var existingListContainer = document.getElementById(containerListID); 	//Get container we are adding nodes to.
+	var endingSpacer = document.getElementById('endingSpacer'); 			//Spacer (hackish) remains at end of list to keep open.
+	var listEntry = document.createElement('li');							//Create node to add.
 
-	/* The li element for our new list entry */
-	var listEntry = document.createElement('li');		
-	/* Insert our new entry before the ending spacer.*/
-	if (locationToAdd == "start"){
-		var entryToPrepend = $('li', existingListContainer)[0];
+	if (locationToAdd == "start"){											//Determine whether we are adding nodes to beginning
+		var entryToPrepend = $('li', existingListContainer)[0];				//	or end of list.  Insert node there.
 		var newEntry = existingListContainer.insertBefore(listEntry, entryToPrepend);
 	} else {
 		var newEntry = existingListContainer.insertBefore(listEntry, linkListEndingSpacer);
 	}
 	
-	
-	
+	//
+	// Creating content to fill the node.
+	//
 	/* The spans inside our new list entry. */
 	var spanForLink = document.createElement('span');
 	var spanForNextIn = document.createElement('span');
@@ -45,7 +38,6 @@ function addLink(containerListName,locationToAdd){
 	/* The input boxes inside the spans of our new list entry */
 	var inputForLink = document.createElement('INPUT');
 	var inputForNextIn = document.createElement('INPUT');
-	var inputForDoEvery = document.createElement('INPUT');
 	var inputForMarkAsDone = document.createElement('INPUT');
 	/* Insert input boxes, this will eventually be replaced with useful things. */
 	spanForLink.appendChild(inputForLink);	
@@ -55,7 +47,6 @@ function addLink(containerListName,locationToAdd){
 	/* Fill in required attributes for above input boxes */
 	inputForLink.setAttribute("Link", "Link"+addedLines);		
 	inputForNextIn.setAttribute("NextIn", "NextIn"+addedLines);
-	inputForDoEvery.setAttribute("DoEvery", "DoEvery"+addedLines);
 	inputForMarkAsDone.setAttribute("MarkAsDone", "MarkAsDone"+addedLines);
 	
 	
@@ -95,66 +86,59 @@ function addIDs(){
 
 function linkObject(name, passedNumber, duration, longName, url){
 	// The main object representing an individual link on the page.
-
+	// There are no defaults since we are creating unique objects.
 	
-	this.name = name;
+	this.name = name;				// Make all arguments properties of the object.
 	this.number = passedNumber;
 	this.duration = duration;
 	this.longName = longName;
 	this.url = url;
 	
-		
-	// Turns the duration passed in, into an expiry date (now + dur seconds)
-	this.expiryDate = function(dur){
-		if (!dur){dur = this.duration;}
-		expires = new Date();
-		Today = new Date();
-		expires.setSeconds(Today.getSeconds() + dur);
-		return expires;
-	}
 	
-	this.cookieExists = function(cookieName){
-		if (!cookieName){cookieName = this.name;}
-		if (document.cookie.indexOf(cookieName) >=0){
-			return true
-		} else {
-			return false
+	/* ------- COOKIE FUNCTIONS ------- */
+		this.expiryDate = function(dur){		// Returns an expiry date based on the current time + dur seconds.
+			if (!dur){dur = this.duration;}
+			expires = new Date();
+			Today = new Date();
+			expires.setSeconds(Today.getSeconds() + dur);
+			return expires;
 		}
-	}
-	
-	// Cookie creation, myValue is usually the duration.  Overwrites existing cookie, use in conjunction with cookieExists() to avoid overwriting.
-	this.createCookie = function(expiresIn){
-		if (!expiresIn){expiresIn = this.duration;}
-		document.cookie = this.name + "=" + this.expiryDate(expiresIn).toUTCString() + "; expires=" + this.expiryDate(expiresIn).toUTCString() + "; path=/";
-	}
-
-	// Delete cookie.  Should check first if cookie exists...
-	this.deleteCookie = function(myValue){	
-		if (!myValue){myValue = this.duration;}
-		var time = new Date()
-		time.setSeconds(time.getSeconds() - 5)
-		document.cookie = this.name + "=" + myValue + "; expires=" + time.toUTCString() + "; path=/";
-	}
-
-	this.readCookie = function(cookieName){
-		if (!cookieName){cookieName = this.name;}
-		var wholeCookie = unescape(document.cookie)
-		var cookies = wholeCookie.split(";")
-		var cookieContents = ""
-		var targetCookie = 0
-		for (i=0;i<cookies.length; i++){
-			if (cookies[i].indexOf(cookieName) >= 0){
-				targetCookie = cookies[i]
-			}}
-		for (i=targetCookie.indexOf("=")+1;i<targetCookie.length;i++){
-			cookieContents = cookieContents + targetCookie[i]
+		this.cookieExists = function(cookieName){
+			if (!cookieName){cookieName = this.name;}
+			if (document.cookie.indexOf(cookieName) >=0){
+				return true
+			} else {
+				return false
+			}
 		}
-		return cookieContents;
-	}
+		this.createCookie = function(expiresIn){
+			if (!expiresIn){expiresIn = this.duration;}
+			document.cookie = this.name + "=" + this.expiryDate(expiresIn).toUTCString() + "; expires=" + this.expiryDate(expiresIn).toUTCString() + "; path=/";
+		}
+		this.deleteCookie = function(myValue){	
+			if (!myValue){myValue = this.duration;}
+			var time = new Date()
+			time.setSeconds(time.getSeconds() - 5)
+			document.cookie = this.name + "=" + myValue + "; expires=" + time.toUTCString() + "; path=/";
+		}
+		this.readCookie = function(cookieName){
+			if (!cookieName){cookieName = this.name;}
+			var wholeCookie = unescape(document.cookie)
+			var cookies = wholeCookie.split(";")
+			var cookieContents = ""
+			var targetCookie = 0
+			for (i=0;i<cookies.length; i++){
+				if (cookies[i].indexOf(cookieName) >= 0){targetCookie = cookies[i]}
+			}
+			for (i=targetCookie.indexOf("=")+1;i<targetCookie.length;i++){
+				cookieContents = cookieContents + targetCookie[i]
+			}
+			return cookieContents;
+		}
 
-	this.destroyLink = function(){
+		this.destroyLink = function(){
 
-	}
+		}
 	
 	
 	// Public for direct use in page, writes the Next In entry.  Basically just a procedure.
@@ -174,9 +158,9 @@ function linkObject(name, passedNumber, duration, longName, url){
 
 var linkList = new Array(
 	new linkObject("healing",0,30*60,"The Healing Springs", "http://www.neopets.com/faerieland/springs.phtml"),
-	new linkObject("mediocrity",1,120,"Wheel of Mediocrity", "http://www.neopets.com/prehistoric/mediocrity.phtml"),
-	new linkObject("excitement",2,240,"Wheel of Excitement")
+	new linkObject("mediocrity",1,40*60,"Wheel of Mediocrity", "http://www.neopets.com/prehistoric/mediocrity.phtml"),
+	new linkObject("excitement",2,120*60,"Wheel of Excitement", "http://www.neopets.com/faerieland/wheel.phtml")
 );
 
-for (i in linkList){
-}
+linkList[linkList.length] = new linkObject("treasure",3,60*60*3,"Buried Treasure", "http://www.neopets.com/pirates/buriedtreasure/index.phtml");
+linkList[linkList.length] = new linkObject("strength",4,60*60*6,"Test Your Strength", "http://www.neopets.com/halloween/strtest/index.phtml");
