eef34440e63f80b35d05ea788d68ef375aa5aee9
rediahs@gmail.com <rediahs@gmail.com>
2011-10-25 22:06:00 -0600

    Using AJAX and JSON to dynamically populate list.  Bug with cookie storage.

--- index.js
+++ index.js
@@ -1,62 +1,5 @@
 var addedLines = 0; //Must be global so that it remains constant outside of functions.
-
-
-// Function that adds a list node.
-// containerListID is the ID (as a string) of the container to add nodes to.
-// If locationToAdd = "start", link is added to start.  Otherwise, added to end.  "end" for convention.
-function addListNode(containerListID,locationToAdd){
-
-	if (!containerListID){containerListID = "linklist"}						//Set default arguments.
-	if ((!locationToAdd) || (locationToAdd != "start" && locationToAdd != "end")){locationToAdd = "end"}
-	document.write(locationToAdd);						////////////////////// *** DEBUG CODE ***
-
-	var existingListContainer = document.getElementById(containerListID); 	//Get container we are adding nodes to.
-	var endingSpacer = document.getElementById('endingSpacer'); 			//Spacer (hackish) remains at end of list to keep open.
-	var listEntry = document.createElement('li');							//Create node to add.
-
-	if (locationToAdd == "start"){											//Determine whether we are adding nodes to beginning
-		var entryToPrepend = $('li', existingListContainer)[0];				//	or end of list.  Insert node there.
-		var newEntry = existingListContainer.insertBefore(listEntry, entryToPrepend);
-	} else {
-		var newEntry = existingListContainer.insertBefore(listEntry, linkListEndingSpacer);
-	}
-	
-	//
-	// Creating content to fill the node.
-	//
-	/* The spans inside our new list entry. */
-	var spanForLink = document.createElement('span');
-	var spanForNextIn = document.createElement('span');
-	var spanForMarkAsDone = document.createElement('span');	
-	/* Insert spans for details to sit in. */
-	newEntry.appendChild(spanForLink);
-	newEntry.appendChild(spanForNextIn);
-	newEntry.appendChild(spanForMarkAsDone);
-	
-	
-	
-	/* The input boxes inside the spans of our new list entry */
-	var inputForLink = document.createElement('INPUT');
-	var inputForNextIn = document.createElement('INPUT');
-	var inputForMarkAsDone = document.createElement('INPUT');
-	/* Insert input boxes, this will eventually be replaced with useful things. */
-	spanForLink.appendChild(inputForLink);	
-	spanForNextIn.appendChild(inputForNextIn);
-	
-	
-	/* Fill in required attributes for above input boxes */
-	inputForLink.setAttribute("Link", "Link"+addedLines);		
-	inputForNextIn.setAttribute("NextIn", "NextIn"+addedLines);
-	inputForMarkAsDone.setAttribute("MarkAsDone", "MarkAsDone"+addedLines);
-	
-	
-	/* addedLines is a variable that keeps track of how many
-	user-added lines there are.  We need this in order to give
-	each added line a unique name automatically.  Otherwise
-	we would need a long list of names in an array and it
-	would not be extendable! */
-	addedLines++;
-}
+var neopetsGMTOffset = 7; // Constant, may change based on Daylight Savings, this is the offset of neopets from GMT.
 
 function formatMS(ms){
 	// Formats a number of milliseconds as hr:mn:sc
@@ -77,31 +20,94 @@ function formatMS(ms){
 	return hr + ":" + mn + ":" + sc
 }
 
-function addIDs(){
-	for (i=0; i<linkList.length(); i++){
-	}
-}
-
-
-
 function linkObject(name, passedNumber, duration, longName, url){
 	// The main object representing an individual link on the page.
 	// There are no defaults since we are creating unique objects.
 	
 	this.name = name;				// Make all arguments properties of the object.
-	this.number = passedNumber;
+	this.numID = passedNumber;
 	this.duration = duration;
 	this.longName = longName;
 	this.url = url;
+	this.containingElementID = "linklist";	// Sets "linklist" as the default ID of list... can change.
 	
 	
 	/* ------- COOKIE FUNCTIONS ------- */
-		this.expiryDate = function(dur){		// Returns an expiry date based on the current time + dur seconds.
-			if (!dur){dur = this.duration;}
-			expires = new Date();
-			Today = new Date();
-			expires.setSeconds(Today.getSeconds() + dur);
-			return expires;
+		this.availableDate = function(dur){		// Returns an expiry date based on the current time + dur seconds.
+			if (!dur){dur = this.duration;}		// Or if the duration is not a time but a fixed date,
+			expires = new Date();				// Return that fixed date, properly formatted as a date.
+			Today = new Date();					// All dates need to be in Neopian time which is PDT!
+			
+	
+	
+			if (dur=="daily"){										// Daily case
+				expires.setSeconds(0);
+				expires.setMinutes(0);
+				expires.setUTCHours(neopetsGMTOffset);
+				if (Today.getUTCHours() >= neopetsGMTOffset){ //Midnight neopets time
+					expires.setDate(Today.getUTCDate() +1);
+				}
+
+				
+			} else if (dur == "monthly"){							// Monthly case
+				expires.setSeconds(0);
+				expires.setMinutes(0);
+				expires.setUTCHours(neopetsGMTOffset);
+				expires.setDate(1);
+				if (Today.getUTCHours() >= neopetsGMTOffset){ //Midnight neopets time
+					expires.setMonth(Today.getUTCMonth()+1);
+				}
+				
+				
+			} else if (dur == "snowager"){								// Snowager case
+				expires.setSeconds(0);									// Can be REALLY confusing.
+				expires.setMinutes(0);									// 6-7am, 2-3pm, and 10-11pm neopian time (PDT)
+
+				
+				//Case after 6am and before 2pm.
+				if (
+					(Today.getUTCHours() >= neopetsGMTOffset + 6) && 
+					(Today.getUTCHours() < neopetsGMTOffset + 14)		// 2pm neopets time is 9pm same day UTC
+				){														// Set expiry to 2pm (9pm same day UTC)
+					expires.setUTCHours(Today.getUTCHours()+neopetsGMTOffset+14);
+				} else
+				
+				
+				//Case after 2pm and before 10pm.
+				if (
+					(Today.getUTCHours() >= neopetsGMTOffset + 14) ||	// We use OR because it spans two days.
+					(Today.getUTCHours() < neopetsGMTOffset -2)			// 2pm neopets time is 9pm same day UTC
+				){														//10pm neopets time is 5am next day UTC								
+					// Set expiry to 5am UTC tomorrow if it is before midnight UTC
+					
+					// Set expiry to 5am UTC same day if it is after midnight UTC.
+				
+				//If it is after 10pm and before 6am.
+					//10pm neopets time is 5am 		next day UTC.
+					// 6am neopets time is 1pm same day UTC
+					// Set expiry to 1pm tomorrow if it is before midnight UTC.
+					// Set expiry to 1pm same day if it is after midnight UTC.
+				}
+			} else if (dur == "decemberdaily"){						// Advent calendar case	
+					expires.setSeconds(0);
+					expires.setMinutes(0);
+					expires.setUTCHours(7);
+				if (Today.getUTCMonth() == 11){	
+					if (Today.getDate() == 31){
+						expires.setFullYear(Today.getFullYear()+1);
+					} else {
+					expires.setDate(Today.getUTCDate() +1);
+					}
+				} else {
+					expires.setDate(1);
+					expires.setMonth(11);
+				}
+				
+				
+			} else {												// Duration as # of seconds case.
+				expires.setSeconds(Today.getSeconds() + dur);
+			}
+				return expires;
 		}
 		this.cookieExists = function(cookieName){
 			if (!cookieName){cookieName = this.name;}
@@ -111,9 +117,9 @@ function linkObject(name, passedNumber, duration, longName, url){
 				return false
 			}
 		}
-		this.createCookie = function(expiresIn){
-			if (!expiresIn){expiresIn = this.duration;}
-			document.cookie = this.name + "=" + this.expiryDate(expiresIn).toUTCString() + "; expires=" + this.expiryDate(expiresIn).toUTCString() + "; path=/";
+		this.createCookie = function(availableIn){
+			if (!availableIn){availableIn = this.duration;}
+			document.cookie = this.name + "=" + this.availableDate(availableIn).toUTCString() + "; expires=" + this.availableDate(availableIn).toUTCString() + "; path=/";
 		}
 		this.deleteCookie = function(myValue){	
 			if (!myValue){myValue = this.duration;}
@@ -124,14 +130,15 @@ function linkObject(name, passedNumber, duration, longName, url){
 		this.readCookie = function(cookieName){
 			if (!cookieName){cookieName = this.name;}
 			var wholeCookie = unescape(document.cookie)
-			var cookies = wholeCookie.split(";")
+			var cookies = wholeCookie.split(';')
 			var cookieContents = ""
 			var targetCookie = 0
 			for (i=0;i<cookies.length; i++){
 				if (cookies[i].indexOf(cookieName) >= 0){targetCookie = cookies[i]}
 			}
-			for (i=targetCookie.indexOf("=")+1;i<targetCookie.length;i++){
-				cookieContents = cookieContents + targetCookie[i]
+			
+			for (j=targetCookie.indexOf("=")+1;j<targetCookie.length;j++){
+				cookieContents = cookieContents + targetCookie[j];
 			}
 			return cookieContents;
 		}
@@ -139,6 +146,67 @@ function linkObject(name, passedNumber, duration, longName, url){
 		this.destroyLink = function(){
 
 		}
+
+
+	// Function that adds the object as a node.
+	// containerListID is the ID (as a string) of the container to add nodes to.
+	// If locationToAdd = "start", link is added to start.  Otherwise, added to end.  "end" for convention.
+	this.addNode = function(containerListID, locationToAdd){
+		if (!containerListID){containerListID = this.containingElementID}						//Set default arguments.
+		if ((!locationToAdd) || (locationToAdd != "start" && locationToAdd != "end")){locationToAdd = "end"}
+
+		var existingListContainer = document.getElementById(containerListID); 	//Get container we are adding nodes to.
+		var endingSpacer = document.getElementById('linkListEndingSpacer'); 	//Spacer (hackish) remains at end of list to keep open.
+		var listEntry = document.createElement('li');							//Create node to add.
+		listEntry.setAttribute('id', this.name);
+
+		if (locationToAdd == "start"){											//Determine whether we are adding nodes to beginning
+			var entryToPrepend = $('li', existingListContainer)[0];				//	or end of list.  Insert node there.
+			var newEntry = existingListContainer.insertBefore(listEntry, entryToPrepend);
+		} else {
+			var newEntry = existingListContainer.insertBefore(listEntry, endingSpacer);
+		}
+		
+		/* Content! */	
+		var spanForLink = document.createElement('span');
+		newEntry.appendChild(spanForLink);
+			var mainHyperlink = document.createElement('a');
+			mainHyperlink.setAttribute('href', this.url);	
+			//mainHyperlink.setAttribute('onClick', 'linkObjects[' + this.numID + '].createCookie()');
+			$(mainHyperlink).click(this.createCookie());
+					if (this.cookieExists()){
+			mainHyperlink.setAttribute('class', "unavailable");
+		}
+			$(mainHyperlink).text(this.longName);
+		spanForLink.appendChild(mainHyperlink);	
+		
+		
+		var spanForNextIn = document.createElement('span');
+			$(spanForNextIn).text(this.nextInString());
+		newEntry.appendChild(spanForNextIn);
+			
+		
+		var spanForMarkAsDone = document.createElement('span');	
+		newEntry.appendChild(spanForMarkAsDone);
+			var buttonMarkAsDone = document.createElement('button');
+				//buttonMarkAsDone.setAttribute('onClick', 'linkObjects[' + this.numID + '].createCookie()');
+				$(buttonMarkAsDone).click(this.createCookie());
+				$(buttonMarkAsDone).text("Done!");
+		spanForMarkAsDone.appendChild(buttonMarkAsDone);
+			var buttonOops = document.createElement('button');
+				//buttonOops.setAttribute('onClick', 'linkObjects[' + this.numID + '].deleteCookie()');
+				$(buttonOops).click(this.deleteCookie());
+				$(buttonOops).text("Oops");
+		spanForMarkAsDone.appendChild(buttonOops);
+		
+		
+	/* addedLines is a variable that keeps track of how many
+	user-added lines there are.  We need this in order to give
+	each added line a unique name automatically.  Otherwise
+	we would need a long list of names in an array and it
+	would not be extendable! */
+	addedLines++;
+	}
 	
 	
 	// Public for direct use in page, writes the Next In entry.  Basically just a procedure.
@@ -147,20 +215,27 @@ function linkObject(name, passedNumber, duration, longName, url){
 		if (this.cookieExists()){
 			var ourCookie = this.readCookie();
 			var ourExpiry = new Date(ourCookie);
-			var formatted = formatMS(ourExpiry.getTime() - Today.getTime());			
-			return formatted;
+			var formattedIt = formatMS(ourExpiry.getTime() - Today.getTime());			
+			return formattedIt;
+			
 		} else {
 			return "00:00:00";
 		}
 	}
 }
 
+// This stuff is a wrapper so I can pass JSON in as arguments to my function! Yay!
+linkObjectTakingJSON = function(obj){
+    for(var o in obj){
+        this[o] = obj[o];
+    }
+};
+linkObjectTakingJSON.prototype = new linkObject();
 
-var linkList = new Array(
-	new linkObject("healing",0,30*60,"The Healing Springs", "http://www.neopets.com/faerieland/springs.phtml"),
-	new linkObject("mediocrity",1,40*60,"Wheel of Mediocrity", "http://www.neopets.com/prehistoric/mediocrity.phtml"),
-	new linkObject("excitement",2,120*60,"Wheel of Excitement", "http://www.neopets.com/faerieland/wheel.phtml")
-);
-
-linkList[linkList.length] = new linkObject("treasure",3,60*60*3,"Buried Treasure", "http://www.neopets.com/pirates/buriedtreasure/index.phtml");
-linkList[linkList.length] = new linkObject("strength",4,60*60*6,"Test Your Strength", "http://www.neopets.com/halloween/strtest/index.phtml");
+var linkObjects = new Array;
+var myJSON = $.getJSON("files.json", function(data){
+	$.each(data, function(index, myObject){
+		linkObjects[index] = new linkObjectTakingJSON(myObject);
+		linkObjects[index].addNode();
+	})
+});
